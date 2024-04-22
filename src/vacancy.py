2f51d67236be6aeecbcab7bf8402ae06d6b910a8
Anton Glazunov <tagl91@yandex.ru>
2024-04-22 11:54:01 +0300

    add vacancy __gt__

--- src/vacancy.py
+++ src/vacancy.py
@@ -5,7 +5,6 @@ class Vacancy:
     alternate_url: str
     requirement: str
     responsibility: str
-    salary = 0
 
     def __init__(self, name, area_name, salary, alternate_url, requirement, responsibility):
         self.name = name
@@ -13,15 +12,21 @@ class Vacancy:
         self.alternate_url = alternate_url
         self.requirement = requirement
         self.responsibility = responsibility
-        self.salary = salary
+        if salary.get("from") in None:
+            self.salary = "Зарплата не указана"
+        else:
+            self.salary = salary.get("from")
 
     def __gt__(self, other):
-        if self.salary != 0:
-            if self.salary > other.selary:
-                return self
-            else:
-                return other
+        if self.salary and other.selary in "Зарплата не указана":
+            return "У предложенных вакансий не указана зарплата"
+        elif self.salary in "Зарплата не указана":
+            return other
+        elif other.salary in "Зарплата не указана":
+            return self
+        elif self.salary > other.salary:
+            return self
         else:
-            return "Зарплата одной из вакансий не указана"
+            return other
 
 
